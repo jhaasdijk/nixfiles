dcf8a5f4056ef0ef0d63a2e07d784bc75cc55bc4
jhaasdijk <j.haasdijk@student.ru.nl>
2019-08-30 22:51:39 +0200

    Update hash due to new st build

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,7 +5,7 @@ self: super:
       owner = "jhaasdijk";
       repo = "st";
       rev = "master";
-      sha256 = "0gppx93rjq475v013q49hcjf80dsbr505sxx71dn227arjq0iw0v";
+      sha256 = "0gbv8s2l8h8vysr6200dk69f06lwaa5z5c3cxdxc2icpldylq2sy";
     };
   });
 }
